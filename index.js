f99cf5ae90527411e76b649dd39379882817e58a
mdpotter31 <mdpotter31@gmail.com>
2023-08-14 20:19:07 -0500

    Added initial functions for the game.

---
+++ index.js
@@ -0,0 +1,21 @@
+const choices = ["rock", "paper", "scissors"];
+
+function game() {
+    playRound();
+}
+
+function playRound() {
+    const playerSelection = playerChoice();
+    const computerSelection = computerChoice();
+}
+
+function playerChoice() {
+    let input = prompt("Type Rock, Paper, or Scissors");
+    input = input.toLowerCase();
+}
+
+function computerChoice() {
+    return choices[Math.floor(Math.random()*choices.length)];
+}
+
+game();
