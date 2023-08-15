2fd124126680742ebff6be4c46c8a09417c2a7ab
mdpotter31 <mdpotter31@gmail.com>
2023-08-14 20:48:15 -0500

    Setup game for just one round in the console.

--- index.js
+++ index.js
@@ -7,15 +7,52 @@ function game() {
 function playRound() {
     const playerSelection = playerChoice();
     const computerSelection = computerChoice();
+    const winner = checkWinner(playerSelection, computerSelection);
+    console.log(winner);
 }
 
 function playerChoice() {
     let input = prompt("Type Rock, Paper, or Scissors");
+    while (input == null) {
+        input = prompt("Type Rock, Paper, or Scissors");
+    }
     input = input.toLowerCase();
+    let check = validatInput(input);
+    while (check == false) {
+   input = prompt(
+    "Type Rock, Paper, or Scissors. Please check your spelling!"
+    );
+    while (input == null) {
+        input = prompt("Type Rock, Paper, or Scissors");
+    }
+    input = input.toLowerCase();
+    check = validatInput(input);
+  }
+  return input;
 }
 
 function computerChoice() {
     return choices[Math.floor(Math.random()*choices.length)];
+}
+
+function validatInput(choice) {
+   return choices.includes(choice);
+}  
+
+
+
+function checkWinner(choiceP, choiceC){
+    if(choiceP === choiceC){
+        return 'Tie'
+    } else if (
+        (choiceP === "rock" && "scissors") || 
+        (choiceP === "paper" && "rock") || 
+        (choiceP === "scissors" && "paper")
+    )  {
+        return 'Player';
+    } else{
+        return "Computer";
+    }
 }
 
 game();
